c5c7c34d970beca2378a8fec6c869356f4fbca11
IvoCostaCunha <ivo.costacunha@icloud.com>
2021-12-09 23:33:51 +0100

    added models + 1 page

---
+++ laravel/app/Models/BornAct.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BornAct extends Model
+{
+    protected $table = 'act_nai3';
+    use HasFactory;
+}
